8f87d05bc3baee3b1e7545db756cfc53f79f3900
faldea1 <faldea1@uc.cl>
2022-12-07 15:55:30 +0000

    start with default

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const MyForm = document.getElementById('myform');
+
+MyForm.addEventListener('submit', (e) => {
+        
+    //primero evitar comportamiento default 
+
+    e.preventDefault();
+
+    //console.log('formulario ha sido enviado');
+
+})
