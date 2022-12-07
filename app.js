87917887f7f5e38d24950d0065c06c5a76b2af8e
faldea1 <faldea1@uc.cl>
2022-12-07 19:57:42 +0000

    Project Form Validation

--- app.js
+++ app.js
@@ -13,72 +13,81 @@ const MyState = document.getElementById('mystate');
 const MyPostalCode = document.getElementById('mypostalcode');
 const MyMessage = document.getElementById('mymessage');
 const MySend = document.getElementById('mysend');
+const MyCancel = document.getElementById('mycancel');
 
 
 //HIDE ALERT MESSAGE
 
 MyAlert.style.display = "none";
 
+
 //FUNCIÓN
 
 MyForm.addEventListener('submit', (e) => {
-        
-    //primero evitar comportamiento default: e.preventDefault();  
+
+    //primero evitar comportamiento default: 
+    //e.preventDefault();  
     //console.log('formulario ha sido enviado');
-    
+
     //show alert message
-    if (SubmitEvent){
+    if (SubmitEvent) {
         MyAlert.style.display = "block";
 
     }
 
-    
     //text content
-    if (FirstName.value === '' || FirstName == null){
+    if (FirstName.value === '' || FirstName == null) {
         e.preventDefault();
-        FirstName.style.backgroundColor = "red";
+        FirstName.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (LastName.value === '' || LastName == null){
+    if (LastName.value === '' || LastName == null) {
         e.preventDefault();
-        LastName.style.backgroundColor = "red";
+        LastName.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (MyCity.value === '' || MyCity == null){
+    if (MyCity.value === '' || MyCity == null) {
         e.preventDefault();
-        MyCity.style.backgroundColor = "red";
+        MyCity.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (MyState.value === '' || MyState == null){
+    if (MyState.value === '' || MyState == null) {
         e.preventDefault();
-        MyState.style.backgroundColor = "red";
+        MyState.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (MyMessage.value === '' || MyMessage == null){
+    if (MyMessage.value === '' || MyMessage == null) {
         e.preventDefault();
-        MyMessage.style.backgroundColor = "red";
+        MyMessage.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
 
     //number content
-    if (MyCard.value === '' || MyCard == null){
+    if (MyCard.value === '' || MyCard == null) {
         e.preventDefault();
-        MyCard.style.backgroundColor = "red";
+        MyCard.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (MyCVC.value === '' || MyCVC == null){
+    if (MyCVC.value === '' || MyCVC == null) {
         e.preventDefault();
-        MyCVC.style.backgroundColor = "red";
+        MyCVC.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (MyAmount.value === '' || MyAmount == null){
+    if (MyAmount.value === '' || MyAmount == null) {
         e.preventDefault();
-        MyAmount.style.backgroundColor = "red";
+        MyAmount.style.backgroundColor = "rgb(248, 215, 218)";
     }
 
-    if (MyPostalCode.value === '' || MyPostalCode == null){
+    if (MyPostalCode.value === '' || MyPostalCode == null) {
         e.preventDefault();
-        MyPostalCode.style.backgroundColor = "red";
+        MyPostalCode.style.backgroundColor = "rgb(248, 215, 218)";
     }
+
+})
+
+//Para resetear ingreso de formulario:
+
+MyCancel.addEventListener('click', _ => {
+    location.reload();
 
 })
